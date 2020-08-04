717967a5e48bd9d3ea029bbfcaa30c2703295018
hamonangan silalahi <yohaneshamonangan@ndamelweb.id>
2020-08-04 09:42:33 +0700

    init

---
+++ api/database/migrations/2020_08_01_030109_create_assigment_submiteds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssigmentSubmitedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assigment_submiteds', function (Blueprint $table) {
+            $table->id();
+            $table->string('file');
+            $table->unsignedInteger('assigment_id');
+            $table->dateTime('submited_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assigment_submiteds');
+    }
+}
