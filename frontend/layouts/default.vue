27445ae065519a22bcbf625f1e42f0cf0efb6265
hamonangan silalahi <yohaneshamonangan@ndamelweb.id>
2020-08-21 21:01:06 +0700

    frontend

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,103 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="miniVariant = !miniVariant" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Dashboard',
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Ini Halaman Dasboard'
+      }
+    ]
+  },
+  data () {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: true,
+      items: [
+        {
+          icon: 'mdi-home',
+          title: 'Dashboard',
+          to: '/student/courses'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: true,
+      right: true,
+      rightDrawer: false,
+      title: 'Virtual School'
+    }
+  },
+}
+</script>
---
+++ frontend/pages/student/courses/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <v-card>
+            <v-card-title>
+                hallo
+            </v-card-title>
+        </v-card>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
