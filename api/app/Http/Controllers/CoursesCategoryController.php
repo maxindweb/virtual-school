2cafb27366615da49f3cd8b0823bd0191e685b2e
hamonangan silalahi <yohaneshamonangan@ndamelweb.id>
2020-09-04 15:31:44 +0700

    edited controller

--- api/app/Http/Controllers/CoursesCategoryController.php
+++ api/app/Http/Controllers/CoursesCategoryController.php
@@ -3,8 +3,55 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\CoursesCategory;
+use Validator;
+use Carbon;
 
 class CoursesCategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(CoursesCategory::all(), 200);
+    }
+
+    public function show(CoursesCategory $category)
+    {
+        return response()->json($category);
+    }
+
+    /**
+     * stored category
+     * @param Request
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'category'  => 'required|'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'error'     => $validator->error()
+            ]);
+        }
+
+        $category  = CoursesCategory::create([
+
+            'category'     => $request->category
+        ]);
+
+        return response()->json([
+            'data'  => $category,
+        ]);
+    }
+
+    public function update()
+    {
+        //
+    }
+
+    public function destroy()
+    {
+        //
+    }
 }
